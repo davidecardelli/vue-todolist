2f0974b5146712e00306bb6ef61397b58be8c56d
davidecardelli <davide.cardelli.94@gmail.com>
2023-01-13 15:37:06 +0100

    add db

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
 console.log('vue ok', Vue);
+
+const app = Vue.createApp({
+
+    data(){
+      return {
+        newItem: '',
+        items: [
+        {
+          text: 'Comprare pane',
+          done: true,
+        },  {
+            text: 'Comprare pasta',
+            done: false,
+        },  {
+            text: 'Pagare la bolletta della luce',
+            done: false,
+        },  {
+            text: 'Ritirare pacco Amazon',
+            done: false,
+        },  {
+            text: 'Passare straccio',
+            done: false,
+        },
+        ]}
+    },
+         methods: {
+
+    },
+}).mount('#root');
